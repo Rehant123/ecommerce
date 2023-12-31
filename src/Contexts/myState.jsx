5c1ea86732cb9fa649668f8a421111b318154975
Rehant123 <rehantthapa.chelsea@gmail.com>
2024-01-01 00:57:26 +0530

    before i make any changes to update

--- src/Contexts/myState.jsx
+++ src/Contexts/myState.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MyContext from "./myContext";
-import { Timestamp } from "firebase/firestore";
-
-
+import { Timestamp, addDoc, deleteDoc, onSnapshot, orderBy, setDoc, updateDoc } from "firebase/firestore";
+import { fireDB } from "../Firebase/Firebaseconfig";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { doc } from "firebase/firestore";
+import {toast} from "react-toastify";
 export default function MyState(props){
     const [mode,setMode] = useState("dark");
     const toggleMode = ()=>{
         if(mode=="dark"){
             setMode("light");
-            document.body.backgroundColor = "rgb(17,24,39)";
+            document.body.style.backgroundColor = "rgb(17,24,39)";
+
         }
         else{
             setMode("dark");
@@ -16,7 +19,8 @@ export default function MyState(props){
         }
     }
     const [loading,setLoading] = useState(false);
-    const [products,setproducts] = useState({
+
+    const [products,setProducts] = useState({
         title:null,
         price:null,
         imageURL:null,
@@ -31,12 +35,122 @@ export default function MyState(props){
         })
     })
 
-    const addproduct = async()=>{
-        
+    
+    //add product 
+    //single product
+
+    const addProduct = async()=>{
+        setLoading(true);
+        console.log(products)
+        if(products.title == null||products.category== null||products.description ==null
+            ||products.imageURL == null||products.price == null){
+                console.log("please enter all the fields")
+                return toast.error("please add all the fields")
+            }
+            try{
+                const productref = collection(fireDB,"products");
+
+                await addDoc(productref,products);
+                toast.success("Product Successfully Added")
+                setTimeout(()=>{
+                    window.location.href = "/dashboard";
+                  },1000)
+                setLoading(false);
+              
+           
+            }catch(error)
+
+            {
+                setLoading(false);
+                console.log("Error")
+            }
     }
 
+    //get product
+    const [product,setProduct] = useState([]);
+
+
+
+
+
+    const getProductData = async()=>{
+        setLoading(true);
+        try{
+            const q = query(collection(fireDB,"products"),orderBy("time"));
+            const querySnapshot = await getDocs(q);
+           let productrarray = [];
+            querySnapshot.forEach((doc) => {
+                console.log(doc)
+           productrarray.push({...doc.data(),id:doc.id});
+
+        })
+        setProduct(productrarray);  
+        setLoading(false)
+        return productrarray;
+        }catch(error){
+            console.log(error);
+            setLoading(false)
+        }
+    }
+
+    //update products
+    const edithandle = (item)=>{
+        setProducts(item);
+    }
+
+
+    const updateProducts = async(item)=>{
+        setLoading(false);
+        console.log(products);
+        try{
+            const productref = doc(fireDB,"products",products.id);
+            //get this specific field 
+
+            await updateDoc(productref,products);
+            //add this products
+            toast.success("Product successfully updated");
+            setLoading(false);
+            getProductData()
+
+
+
+        }catch(error){
+            console.log(error);
+            setLoading(false);
+            
+        }
+
+
+    }
+
+    //delete products
+    
+    const deleteProduct = async(item)=>{
+        console.log(item)
+        
+        console.log("you called me")
+        setLoading(true);
+        try{
+            const productRef = doc(fireDB,"products",item.id);
+            await deleteDoc(productRef);
+        
+           toast.success("product deleted successfully");
+           setLoading(false);
+           getProductData();
+
+        }catch(error){
+            console.log(error)
+            setLoading(false);
+
+        }
+
+    }
+
+    useEffect(()=>{
+        getProductData();
+    },[]);
     return (
-        <MyContext.Provider value = {{mode,toggleMode,loading,setLoading}}>
+        <MyContext.Provider value = {{mode,toggleMode,loading,setLoading,product,setProducts,products,addProduct,updateProducts,edithandle,deleteProduct}}>
         {props.children}
         </MyContext.Provider>
     )   
